2fc046e7c77920e9095e5db3a83240fd78387d14
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-04 14:57:11 +0900

    add user_model

---
+++ db/migrate/20170204054618_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default: false, null: false
+    add_column :users, :name, :string
+    add_column :users, :user_image, :string
+  end
+end
