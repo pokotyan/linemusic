bc7841c5eb24f32f83e09bcc82d32ae4b17eae4e
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-05 08:34:01 +0900

    add function for play music
    songs#play、songs#set_music_infoはajaxから叩く。
    songs#playはaudioタグのsrcにmp3のパスを入れて再生させる。
    songs#set_music_infoはプレイヤー上の曲名、アーティスト名を更新及び、titleタグの更新を行う。

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,20 @@ class SongsController < ApplicationController
     end
   end
 
+  def play
+    song = Song.find(params[:music_id])
+    song.increment!(:play_count) #再生回数をインクリ
+    #audioタグのsrcへ直接パスを入れるのでダイジェストがついたパスを用意しなければいけないのでasset_pathを使う。
+    #Rails.logger.info ActionController::Base.helpers.asset_path(music.path) デバッグ用
+    render :text => ActionController::Base.helpers.asset_path(song.path)
+  end
+
+  def set_music_info
+    song = Song.find(params[:music_id])
+    @song_name = song.name + " "           #表示するときに曲名とアーティスト名に空白を設けたいのでここで設定しとく
+    @artist_name = song.album.artist.name
+  end
+
   private
 
   def song_params
