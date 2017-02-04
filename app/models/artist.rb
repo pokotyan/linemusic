9496999c42648d2283e0ddf17f6c30f8f2863e73
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-04 18:03:41 +0900

    Add function for Artist posting

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,7 @@ class Artist < ActiveRecord::Base
   belongs_to :genre
   has_many :albums, dependent: :destroy
 
+  validates :genre_id,:name, presence: true
+
   mount_uploader :artist_image, ArtistImageUploader
 end
