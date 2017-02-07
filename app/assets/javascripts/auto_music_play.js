8ee523338a912cec83a55723411c285d29622138
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-07 13:04:32 +0900

    remove backslash for javascript
    ec2でプリコンパイルした時に以下のエラーが発生
    rake aborted!
    ExecJS::RuntimeError: SyntaxError: Unexpected character '`' (line: 14868, col: 16, pos: 410426)
    
    「予期しないバックスラッシュ」というエラーなのでバックスラッシュを使わないようにjsを書き換え。

--- app/assets/javascripts/auto_music_play.js
+++ app/assets/javascripts/auto_music_play.js
@@ -3,7 +3,7 @@ $(document).on('ready pjax:success',function(){
   $(".tr-a-play").on('click', function(e) {
     var all_music_ids = $('table').data('musicsId');      //曲のid一覧の配列
     var click_music_id = $(this).data('songId');          //クリックしたの曲のid
-    console.log(`全曲は${all_music_ids}`);
+    console.log("全曲は" + all_music_ids);
 
     //クリックした曲"以降"の残りの曲idの配列の作成
     var nokori_music_ids = all_music_ids.slice();         //配列のコピー
@@ -12,7 +12,7 @@ $(document).on('ready pjax:success',function(){
     }
 
     //連続再生
-    console.log(`選んだ曲は${click_music_id} 残りの曲は${nokori_music_ids} つぎの曲は${nokori_music_ids[0]}`);
+    console.log("選んだ曲は" + click_music_id + " 残りの曲は" + nokori_music_ids + " つぎの曲は" + nokori_music_ids[0]);
     $('#song').on('ended', function (e) {                 //曲が終わったら
       if(nokori_music_ids.length != 0){                   //残りの曲があれば
         play_music(nokori_music_ids[0]);                  //残りの曲idの配列の先頭を再生
