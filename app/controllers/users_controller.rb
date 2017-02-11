fa19c30ed514dc9cfae737a8b48d47e3681edfd9
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-11 16:27:22 +0900

    display favorite songs to mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    #お気に入りソング
+    @songs = @user.f_songs
+    @songs_id = @user.f_songs.map{|s|s.id}
   end
 end
