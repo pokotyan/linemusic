66158e5f0088d62738081126ed1a6b99f2df5bca
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-11 14:42:56 +0900

    close modal on pjax

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,4 +2,8 @@ $(function() {
   $(window).load(function() {
     $('#ticket_modal_trigger').click();
   });
+  //pjaxでログイン画面に遷移するとモーダルが開きっぱなしなので閉じる
+  $(".modal_login_btn").click(function(){
+    $(".modal-header > .close").click();
+  });
 });
