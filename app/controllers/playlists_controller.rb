1d01cb7c6d92584ddd59e93ae9a8443d1556d4bf
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-02-26 16:04:52 +0900

    add function for add song to playlist
    - なぜプレイリスト作成のjsをapplication.html.hamlにインラインで書いているかについて
    もともとプレイリスト作成のjsは、ドロワーメニューの各プレイリストのクリックハンドラで書いていたが、
    いろんなページ（pjax遷移）で曲の選択/選択解除を繰り返した上でドロワーメニューのプレイリストをクリックしたら
    clickイベントが多重登録されてるせいで、イベントの数だけクリックハンドラを実行した。（つまりajaxリクエストをイベントの数だけ何回も送信した）
    クリックイベントを多重登録させないためにjqueryでclickイベントのoff/onをやったりしたが、この方法だと
    初回クリックがきかなくなる。で、初回クリックをjs側でやろうとしたけどうまくいかなかった。
    また、jqueryのクリックイベントハンドラではなく、addEventListenerでクリックのリスナーを書いてみた（addEventListenerはイベントが多重登録されないらしい）けど、普通に多重登録された。
    なので、assets/javascript配下にjsファイルを配置するのではなく、application.html.hamlにインラインで書いてonclick属性で実行させるようにした。
    これならajaxでページ遷移しまくっても、そもそもclickイベントを登録してないので処理が多重実行されることはなくなった。

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,11 @@
 class PlaylistsController < ApplicationController
+  def create
+    #params[:data]はこんな感じのデータ {"song_id"=>["26", "23", "7", "5"], "playlist_id"=>"1"}
+    song_ids = params[:data][:song_id].map(&:to_i)
+    playlist_id = params[:data][:playlist_id].to_i
+    song_ids.each do |song_id|
+      PlaylistSong.find_or_create_by(playlist_id:playlist_id,song_id:song_id) #なければ作成。同じ曲を重複してプレイリストに入れない
+    end
+    @playlist = Playlist.find(playlist_id)
+  end
 end
