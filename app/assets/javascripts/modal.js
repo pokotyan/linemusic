28059e92d52d72d69145c9704c5fc2b44a0e7f28
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-03-07 20:50:27 +0900

    fix ticket modal
    「まだログインしたことがないユーザに30日間の無料チケットをプレゼント!」というモーダルが
    ログイン後も表示されるのはおかしいのでログイン時は表示しないように修正

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,8 @@
 $(function() {
   $(window).load(function() {
-    $('#ticket_modal_trigger').click();
+    if( !$(".header-user-image")[0] ){     //ログインしてなかったら
+      $('#ticket_modal_trigger').click();  //チケットモーダルを表示
+    }
   });
   //pjaxでログイン画面に遷移するとモーダルが開きっぱなしなので閉じる
   $(".modal_login_btn").click(function(){
