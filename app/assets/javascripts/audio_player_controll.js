662e64023c1246b5bf242c7899ba47aaafe9c7bd
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-03-13 18:54:26 +0900

    refactaling click handler
    .prevと.nextのクリックハンドラで共通している処理の部分はまとめ、違う部分は$(this).hasClassで分岐させるようにリファクタリング。

--- app/assets/javascripts/audio_player_controll.js
+++ app/assets/javascripts/audio_player_controll.js
@@ -14,40 +14,32 @@ $(document).on('ready pjax:end',function(){
     current = all_music_ids.indexOf(click_music_id);            //現在の再生曲の配列での位置（添字）を保存,グローバル
   });
 
-  $(".prev").on("click",function(){                             //戻るを押した時
-    $(".prev").off('click');
-    $(".prev").on("click",function(){
-      if(typeof random !== 'undefined' && random){              //ランダムが定義されていてかつ、有効なら
-        return current = random_play(all_music_ids,current);    //ランダム再生
-      }
+  $(".prev,.next").on("click",function(){                       //前へ、もしくは次へを押した時
+    $(".prev,.next").off('click');
+    $(".prev,.next").on("click",function(){
       if(all_music_ids.length === 0){                           //再生する曲がない時は
         return;                                                 //何もしない
       }
-      if(current === 0){                                        //今が１曲目なら
-        return play_music(all_music_ids[current]);              //prev_songがないので再生し直す
-      }
-      current = current - 1                                     //前の曲を再生
-      play_music(all_music_ids[current]);
-    });
-  });
-
-  $(".next").on("click",function(){                             //次へを押した時
-    $(".next").off('click');
-    $(".next").on("click",function(){
       if(typeof random !== 'undefined' && random){              //ランダムが定義されていてかつ、有効なら
         return current = random_play(all_music_ids,current);    //ランダム再生
       }
-      if(all_music_ids.length === 0){                           //再生する曲がない時は
-        return;                                                 //何もしない
-      }
-      if( current === (all_music_ids.length - 1) ){             //今が最後の曲なら
-        if(typeof repeat_all !== 'undefined' && repeat_all){       //リピートが定義されていてかつ、有効なら
-          return current = repeat(all_music_ids,current);          //リピート再生
+      if($(this).hasClass("prev")){                             //押したボタンが前への場合、
+        if(current === 0){                                        //今が１曲目なら
+          return play_music(all_music_ids[current]);              //prev_songがないので再生し直す
         }
-        return play_music(all_music_ids[current]);              //next_songがないので再生し直す
+        current = current - 1                                   //前の曲を再生
+        play_music(all_music_ids[current]);
       }
-      current = current + 1                                     //次の曲を再生
-      play_music(all_music_ids[current]);
+      if($(this).hasClass("next")){                             //押したボタンが次への場合、
+        if( current === (all_music_ids.length - 1) ){             //今が最後の曲なら
+          if(typeof repeat_all !== 'undefined' && repeat_all){       //リピートが定義されていてかつ、有効なら
+            return current = repeat(all_music_ids,current);          //リピート再生
+          }
+          return play_music(all_music_ids[current]);              //next_songがないので再生し直す
+        }
+        current = current + 1                                   //次の曲を再生
+        play_music(all_music_ids[current]);
+      }
     });
   });
 });
