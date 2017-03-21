41e7a81f06205e296a5dc4ef82f5480032b926ea
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-03-21 13:50:24 +0900

    fix bug for trend_page
    pjax:successだとコンテンツの表示切り替え（bootstrapのtab）がされなかったが、pjax:endに変更したらちゃんと切りかわった。

--- app/assets/javascripts/trend.js
+++ app/assets/javascripts/trend.js
@@ -4,11 +4,11 @@ $(document).on('ready pjax:success',function(){
   $(".more").on('click', function(e) {
     var title = $(e.target).parents('.title_bar').find("h3").text();
     if( title == "NEW ALBUMS" ){
-      $(document).on('ready pjax:success',function(){
+      $(document).on('ready pjax:end',function(){
         $("a[href = '#new_albums']").click();
       });
     }else if( title == "おすすめアーティスト" ){
-      $(document).on('ready pjax:success',function(){
+      $(document).on('ready pjax:end',function(){
         $("a[href = '#reccomend_artists']").click();
       });
     }
