637bdba33e3715b0462c2afa10ee3831e45871f7
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-04-17 08:36:12 +0900

    fix bug for facebook comment plugin
    pjax遷移時もコメントプラグインを表示させるため、明示的にFB.XFBML.parse();を呼んでたが、
    readyイベントの時はFB.XFBML.parse();を呼んではダメ。カルーセルが崩れる。pjax:endのイベントで呼ぶこと。
    またpjax:endで呼ぶこと。そうしないとpjaxが完了しなくなる。

--- app/assets/javascripts/facebook_comment.js
+++ app/assets/javascripts/facebook_comment.js
@@ -1,6 +1,5 @@
-$(document).on('ready pjax:success',function(){
+$(document).on('ready pjax:end',function(){
   (function(d, s, id) {
-    //FB.XFBML.parse();
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s);
@@ -8,5 +7,9 @@ $(document).on('ready pjax:success',function(){
     js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8&appId=228314570969832";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+});
+//readyイベントの時はFB.XFBML.parse();を呼んではダメ。カルーセルが崩れる
+//pjax:endのイベントで呼ぶこと。またendで呼ばないとpjaxが完了しなくなる
+$(document).on('pjax:end',function(){
   FB.XFBML.parse();
 });
